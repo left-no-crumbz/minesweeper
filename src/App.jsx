75fe1ae31eed2a7d5f3bea48df55e2747a433586
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2025-06-28 23:09:54 +0800

    feat: Implement scoreboard, status, and turn switching

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,44 @@
 import './App.css'
-
-function generateGrid() {
-  const tiles = [];
-  for (let i = 0; i < 36; i++) {
-    tiles.push(
-      <button className='tile' key={i}>
-        {i & 0x1 ? "Chicken": "Banana"}
-      </button>
-    );
-  }
-  return tiles;
-}
+import { useState } from 'react';
 
 function App() {
+  const [player, setPlayer] = useState('Chicken');
+
+  function switchPlayer() {
+    setPlayer(player === 'Chicken' ? 'Banana' : 'Chicken');
+  }
+
+  function generateGrid() {
+    const tiles = [];
+    for (let i = 0; i < 36; i++) {
+      tiles.push(
+        <button className='tile' key={i} onClick={switchPlayer}>
+          {i + 1}
+        </button>
+      );
+    }
+    return tiles;
+  }
+
+
   return (
     <>
-      <h1>hello world</h1>
+      <div className="header">
+        <div className="scoreboard">
+          <h3 className='scoreboard-title'>Scoreboard</h3>
+          <p className='chicken-score'>0</p>
+          <p className='banana-score'>0</p>
+        </div>
+
+        <div className="game-status">
+          <h3 className='scoreboard-title'>Game Status</h3>
+          <p>Two players: <b>Chicken</b> and <b>Banana</b></p>
+          <div className="turn">{player}'s Turn</div>
+        </div>
+      </div>
       
+      <br />
+
       <div className="grid">
         {generateGrid()}
       </div>
